0d293ab5552681b3c204de7b580bf6c497c0ba10
Camilaux <111092335+Camilaux@users.noreply.github.com>
2022-09-22 22:16:20 -0500

    Update Animal.java

--- src/main/java/zooAnimales/Animal.java
+++ src/main/java/zooAnimales/Animal.java
@@ -21,6 +21,7 @@ public class Animal {
 		this.edad = edad;
 		this.habitat = habitat;
 		this.genero = genero;
+		totalAnimales ++;
 	}
 	
 	public String movimiento() {
@@ -28,9 +29,9 @@ public class Animal {
 	}
 	
 	public static String totalPorTipo() {
-		String texto = "Mamiferos: " + Mamifero.cantidadMamiferos() + "\nAves: " + Ave.cantidadAves() +
-				"\nReptiles: " + Reptil.cantidadReptiles() + "\nPeces: " + Pez.cantidadPeces() +
-				"\nAnfibios: " + Anfibio.cantidadAnfibios();
+		String texto = "Mamiferos: " + Mamifero.cantidadMamiferos() + "\n" + "Aves: " + Ave.cantidadAves() +
+				"\n" + "Reptiles: " + Reptil.cantidadReptiles() + "\n" + "Peces: " + Pez.cantidadPeces() +
+				"\n" + "Anfibios: " + Anfibio.cantidadAnfibios();
 		return texto;
 	}
 	
